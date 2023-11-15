4bbfbbedabef1cde2948dc43f4a0d4fd4c2aa331
Bread <103989916+BreadTeleporter@users.noreply.github.com>
2023-11-15 09:24:09 +0000

    Add files via upload

---
+++ test/upload.php
@@ -0,0 +1,19 @@
+<?php
+
+require("../alib.php");
+$ALIB = new alib();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if($ALIB->uploadToDir($_FILES["userfile"], "upload-directory/")){
+        echo "Pass!";
+    } else {
+        echo "Fail!";
+    }
+}
+
+?>
+
+<form enctype="multipart/form-data" method="post">
+    <input type="file" name="userfile" id="file">
+    <input type="submit" value="Upload">
+</form>
